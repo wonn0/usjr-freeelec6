b216ca915ebba9c41cb1a75dbeaea9c915b02907
Garfield Lim <garfield.lim@usjr.edu.ph>
2023-11-08 23:30:10 +0800

    book model integrated (needs reviews)

--- ASI.Basecode.WebApp/Controllers/HomeController.cs
+++ ASI.Basecode.WebApp/Controllers/HomeController.cs
@@ -3,7 +3,13 @@ using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using ASI.Basecode.Services.Models;
+using ASI.Basecode.WebApp.Services;
 using Microsoft.Extensions.Logging;
+#nullable enable // This enables nullable reference types for this file
+using System;
+
+
 
 namespace ASI.Basecode.WebApp.Controllers
 {
@@ -20,12 +26,14 @@ namespace ASI.Basecode.WebApp.Controllers
         /// <param name="configuration"></param>
         /// <param name="localizer"></param>
         /// <param name="mapper"></param>
-        public HomeController(IHttpContextAccessor httpContextAccessor,
+        private readonly IBookService _bookService;
+        public HomeController(IHttpContextAccessor httpContextAccessor, IBookService bookService,
                               ILoggerFactory loggerFactory,
                               IConfiguration configuration,
                               IMapper mapper = null)
             : base(httpContextAccessor, loggerFactory, configuration, mapper)
         {
+            _bookService = bookService ?? throw new ArgumentNullException(nameof(bookService));
             // Assuming _logger is initialized in the base class.
         }
 
@@ -40,14 +48,18 @@ namespace ASI.Basecode.WebApp.Controllers
 
             return View();
         }
-        public IActionResult ViewBook()
+        [HttpPost]
+        public IActionResult ViewBook(int id)
         {
-            // Use _logger to log the information
-            _logger.LogInformation("ViewBook page visited at {Time}", System.DateTime.Now);
+            var viewModel = _bookService.GetBookById(id);
+            if (viewModel is null)
+            {
+                _logger.LogWarning("Book with ID {BookId} not found.", id);
+                return NotFound();
+            }
 
-            return View();
+            return View(viewModel);
         }
-
         // ... other actions
     }
 }
