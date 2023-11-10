519461f68b719108b7da5e6d485f673cfe9f0931
wonn0 <113926116+wonn0@users.noreply.github.com>
2023-11-10 15:31:49 +0800

    change datatype of book name

--- ASI.Basecode.Services/ServiceModels/BookReviewViewModel.cs
+++ ASI.Basecode.Services/ServiceModels/BookReviewViewModel.cs
@@ -41,7 +41,7 @@ namespace ASI.Basecode.Services.Models
         /// <summary>Book (Model)</summary>
         [JsonPropertyName("bookName")]
         [Required(ErrorMessage = "Book is required.")]
-        public BookViewModel BookName { get; set; }
+        public String BookName { get; set; }
     }
 }
 
